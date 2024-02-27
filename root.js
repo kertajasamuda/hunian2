bab5c81b106455f59cdef71460bc39a8fa025d71
kertajasamuda <160152667+kertajasamuda@users.noreply.github.com>
2024-02-27 14:31:19 +0000

    Update root.js

--- root.js
+++ root.js
@@ -77,7 +77,7 @@ async function startBrowser(data) {
                 '--disable-setuid-sandbox',
                 '--ignore-certificate-errors',
                 '--ignore-certificate-errors-skip-list',
-                `--proxy-server=socks5://209.38.204.166:30002`,
+                `--proxy-server=socks5://143.198.26.125:44444`,
                 '--disable-dev-shm-usage'
             ]
         })
